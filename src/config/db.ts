1a1ddef3ee3200734effe6296c9ae5bf50c18636
Magaerv <marielaramirezvalle@gmail.com>
2024-08-16 10:43:51 -0300

    config deployment

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-	dialect: 'postgres',
 	models: [__dirname + '/../models/**/*'],
 	logging: false,
+	dialectOptions: {
+		ssl: {
+			require: 'true',
+		},
+	},
 });
 
 export default db;
